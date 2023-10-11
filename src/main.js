4d0dd712f6124fa3e6b1df824aff9682264e4691
Hareessa <hareessaabu@gmail.com>
2023-10-12 06:59:32 +0800

    installing vercel analytic

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+import { inject } from '@vercel/analytics';
+
+inject();
 
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
